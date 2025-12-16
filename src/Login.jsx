22fb2fe5a8e280dfecb4c35038919c44abd93197
Antonyvijay123 <antonyvijay.cd22@bitsathy.ac.in>
2025-12-16 11:19:38 +0530

    Login Started

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+
+    <div>
+        
+        <div className='shadow' style={{width:"30%", margin:"auto"}}>
+       
+        <form className="my-5" style={{width:"50%", margin:"auto"}}>
+            <h4 style={{"color":"green"}} className='ms-5 my-5'>Login</h4>
+            <label>Email :</label><input type="text" className='form-control my-3' />
+            <label>Password :</label><input type="password" className='form-control my-3' />
+            <button className='btn btn-primary mx-5 my-3 px-3'> Login</button>
+        </form>
+
+         </div>
+    </div>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='d-flex mt-5'>
-        
+    <div className='d-flex justify-content-around ms-3'>
+        <div className='d-flex gap-1 ' style={{"backgroundColor":"white"}}> 
+            <i className="bi bi-house-door-fill" style={{"backgroundColor":"white"}}></i>
+            <p style={{"backgroundColor":"white"}}>Rently & Co</p>
+            <div className='d-flex gap-5 ms-5' style={{"fontSize":"20px","backgroundColor":"white"}}>
+                    <p style={{"backgroundColor":"white"}}>Home</p>
+                    <p style={{"backgroundColor":"white"}} >Buy</p>
+                    <p style={{"backgroundColor":"white"}}>Rent</p>
+                    <p style={{"backgroundColor":"white"}}>Sell</p>
+            </div>
+        </div>
     </div>
   )
 }
