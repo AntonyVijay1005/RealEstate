b167004fc701a4ec26f647535d513b3eb5713370
Antonyvijay123 <antonyvijay.cd22@bitsathy.ac.in>
2025-12-23 21:01:49 +0530

    changes added

--- src/Property.jsx
+++ src/Property.jsx
@@ -66,9 +66,9 @@ function Property() {
 
         <Navbar />
 
-        <div className='container mt-5'>
+        <div className='container mt-5 '>
              
-            <div className='row'>
+            <div className='row d-flex justify-content-center'>
 
             <div className='col-12 col-md-6 col-lg-6 d-flex'>
               <img src={room3} className='prop-img w-100 h-100 p-3' />
@@ -92,14 +92,49 @@ function Property() {
 
       <div className='row mt-5 d-flex justify-content-center'>
 
-      
-        <div className='col-12 col-md-6 col-lg-6 predict p-3 border border-2 shadow d-flex  rounded d-flex flex-column  justify-content-center align-item-center' style={{backgroundColor:"white",height:"300px" ,width:"600px"}}>
+      <div className='col-12 col-md-6 col-lg-5 p-5 border border-2 shadow rounded d-flex flex-column'>
+
+      <h3>About Property </h3>
+
+      <p>Spacious modern villas located in a premium residential area, featuring contemporary architecture, private parking, landscaped gardens, and excellent connectivity to schools and IT hubs.Compact and affordable flats perfect for working professionals and small families, located close to public transport, shopping centers, and business districts.</p>
+     
+       <h5>Features</h5>
+
+        <div className='container'>
+
+          <div className='d-flex flex-row justify-content-around gap-4'>
+
+            <i className="bi bi-lamp fs-1"></i>
+            <i className="bi bi-droplet fs-1"></i>  
+            <i class="bi bi-building-fill-gear fs-1"></i>
+          </div>
+
+          <div className='d-flex flex-row justify-content-around gap-4'>
+
+            <p>2 beds</p>
+            <p>3 rooms</p>
+            <p>4 years</p>
+
+          </div>
+
+        </div>
+      </div>
+        
+        
+        
+        
+        <div className='col-12 col-md-6 col-lg-6 predict p-5 border border-2 shadow d-flex  rounded d-flex flex-column  justify-content-center align-item-center' style={{backgroundColor:"white",height:"300px" ,width:"600px"}}>
 
               <h2>Estimated <span className='text-success'>Price</span>    </h2>
                <input className='mt-4' type="number" placeholder='Enter Number of Years to find the predicted price' name="rate" onChange={handleChange} />
               <h4 className='mt-4 text-primary d-flex justify-content-center' ><i className="bi bi-currency-rupee"></i>{predicted_price}</h4>
               <button className='btn btn-primary mt-4' onClick={()=>predict()}>Predict now</button>
         </div>
+
+        </div>
+
+        <div className='row'>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur fugit maiores repudiandae neque deserunt velit quod corrupti ducimus at tempora dolor, dolorum saepe, veniam est accusantium ea laboriosam autem?
 
         </div>
         </div>
